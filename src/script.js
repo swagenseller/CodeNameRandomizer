2f7c722e1806ef85081406dda7cd421e32129caf
Jeff <jwagense@ucsc.edu>
2020-03-28 10:38:58 -0700

    programmed the board to set random colors

--- src/script.js
+++ src/script.js
@@ -1,13 +1,14 @@
 // create a 5 by 5 board
 var b = document.querySelector("#board")
 
+
 var board = new Array(5);
-for (var z = 0; z< 5; z++){
+for (let z = 0; z< 5; z++){
     board[z] = new Array(5);
 }
 //b.append("tr").attr("id", "test")
 addCells();
-
+randBoard();
 
 function addCells(){
 
@@ -19,10 +20,51 @@ function addCells(){
             let td = document.createElement("td");
             td.setAttribute("id", "" + i + j);
             newRow.append(td);
+            board[i][j] = td;
         }
         b.append(newRow)
     }
    
-
-
 }
+
+// randomize the board 
+// colors: 17 red & blue squares, 7 tan & 1 black square
+function randBoard(){
+    //create an array with 0-24 in a random order
+    let colorBoard = [];
+    let first;
+    let second;
+    while(colorBoard.length < 25){
+        let r = Math.floor(Math.random() * 25);
+        if(colorBoard.indexOf(r) === -1) {
+            colorBoard.push(r);
+        }
+    }
+    // first determine blue or red goes first
+    let sign = Math.floor(Math.random() * 2); // 0 blue, 1 red
+    if (sign < 1){
+        first = "blue";
+        second = "red";
+    } else {
+        first = "red";
+        second = "blue";
+    }
+
+    // assign colors to each one based values in colorBoard
+    let k = 0;
+    for (let i =0; i< board.length; i++ ){
+        for(let j =0; j<board[0].length; j++){
+            if (colorBoard[k] >= 16 ){
+                board[i][j].style.backgroundColor = first;
+            } else if (colorBoard[k] < 16 && colorBoard[k] >= 8){
+                board[i][j].style.backgroundColor = second;
+            } else if(colorBoard[k] === 0){
+                board[i][j].style.backgroundColor = "black";
+            }
+            k++;
+        }
+    }
+} 
+    
+
+
